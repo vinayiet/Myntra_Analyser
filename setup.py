2a07c6511ee78de15273f522fe276591980b6173
Vinay Sharma <vinayiet435@gmail.com>
2024-09-25 13:06:13 +0530

    this is the first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='scrapper',
+    version='0.0.1',
+    author_email='vinayiet435@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
