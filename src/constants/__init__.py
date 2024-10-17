43a460f47ee23b0f4950d75447d86b4c4595730c
Vinay Sharma <vinayiet435@gmail.com>
2024-10-17 09:20:46 +0530

    made changes into the cloud_io

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,4 @@
-MONGODB_URL: str = ""
-MONGODB_DATABASE_NAME: str = ""
+MONGODB_URL: str = "mongodb+srv://myntra:myntra123@cluster0.zumvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_DATABASE_NAME: str = "myntra-reviews"
 
 SESSION_PRODUCT_KEY: str = "product"
